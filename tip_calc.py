3771e14786f7d54ac04d936afa491cbda1281bd1
Travisthefish <148793709+smallbigie@users.noreply.github.com>
2025-02-26 15:42:01 +0000

    Work done on WED, FEB 26 (Excluding UML for now)

--- tip_calc.py
+++ tip_calc.py
@@ -1,3 +1,42 @@
 # tip_calculator.py
 # A simple tip calculator to compute tip amount and total bill
 
+#Welcome 
+
+
+################## MOST OF THIS IS JUST UNFINISHED PSEUDO CODE I COPIED FROM THE txt FILE ##########################
+
+def tip_calculator():
+    print ("Welcome to the Tip Calculator")
+
+
+    #Ask for Bill Amount    #bill_amount
+    "Please enter your Bill Amount: "
+
+
+    #Validate
+    If Bill Amount <0                   <-- Needs fixing later
+        PRINT "Invalid ..."             <-- Needs fixing later
+
+
+    #Ask for Tip Amount     #tip_amount
+    PRINT "Please enter the amount you would like to tip (e.g. 10, 15, 20, 25): "
+
+
+    #Validate Tip Amount
+    If Tip Amount (is not percentage)   <-- Needs fixing later
+
+
+    #Calculate the Amounts
+    bill_amount * tip_percentage = tip_amount
+    total_bill = bill_amount + tip_amount
+
+
+    #Display Results
+    print bill_amount: (e.g., two decimal places and a dollar sign)
+    print tip_amount: (e.g., two decimal places and a dollar sign)
+    print total_bill: (e.g., two decimal places and a dollar sign)
+
+
+    #Thanks
+    PRINT "Thank you for using the Tip Calculator"
